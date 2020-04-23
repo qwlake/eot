4c8be85ca4ad7fb996786ee6fe0deb258cc176ee
qwlake <qwlake@gmail.com>
2020-04-23 09:16:07 +0900

    edit base, resume_result

--- src/users/views.py
+++ src/users/views.py
@@ -4,16 +4,16 @@ from .models import User
 from docxmerge.models import ResumeInfo, ResumeMerged, Resume
 
 def mypage(request):
-    usercoin = request.user.usercoin
-    resume_merged_list = []
-    resume_info_list = ResumeInfo.objects.filter(user=request.user)
-    return render(request, 'mypage.html', {'usercoin':usercoin, 'resume_info_list':resume_info_list})
+    if request.user.is_authenticated:
+        usercoin = request.user.usercoin
+        resume_info_list = ResumeInfo.objects.filter(user=request.user)
+        return render(request, 'mypage.html', {'usercoin':usercoin, 'resume_info_list':resume_info_list})
+    else:
+        return redirect('users:login')
 
 def coin_add(request, amount):
     request.user.coin_add(amount)       # 코인 증가
     return redirect('users:mypage')
-
-
 
 def coin_sub(request, amount):
     request.user.coin_sub(amount)          #코인 감소
