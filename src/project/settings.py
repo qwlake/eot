f329274a0372e0d1f82c73dc458b9b1ef13e8887
qwlake <qwlake@gmail.com>
2020-04-29 01:52:34 +0900

    hotfix media connect

--- src/project/settings.py
+++ src/project/settings.py
@@ -35,7 +35,9 @@ SECRET_KEY = get_secret('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'resume-make.shop',
+]
 
 
 # Application definition
